c7968bf03904c8cc3be7d245287309084e2c40f6
mantisa1980 <mantisa1980@gmail.com>
2017-10-25 23:00:25 +0800

    add unit test scripts

---
+++ mantisa/sample_code/src/unittest/script1_test.go
@@ -0,0 +1,27 @@
+package unittest
+import ( "testing")
+
+func Test_foo(t* testing.T) {
+    A:=1
+    if A != 1 {
+        t.Error("Expected:", 1, " Got:", A)
+    }
+
+    if foo_subtest1(1,2) != 3 {
+        t.Error("Expected:", 3, " Got:", foo_subtest1(1,2) )
+    }
+    if foo_subtest2(1,2) != 3 {
+        t.Error("Expected:", 3, " Got:", foo_subtest2(1,2) )
+    }
+}
+
+func Test_bar(t* testing.T) {
+    t.Error("err msg1", "err msg2", "err msg 3")
+}
+
+func foo_subtest1(a1 int, a2 int) int {
+    return a1+a2
+}
+func foo_subtest2(a1 int, a2 int) int {
+    return a1-a2
+}
---
+++ mantisa/sample_code/src/unittest/script2_test.go
@@ -0,0 +1,9 @@
+package unittest
+
+import ( "testing")
+
+func Test_qaz(t* testing.T) {
+}
+func test_not_run(t* testing.T) {
+}
+    
