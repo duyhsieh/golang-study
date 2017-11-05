3db9fcb3fc63d071db1aa711ecf895416990518f
mantisa1980 <mantisa1980@gmail.com>
2017-11-06 01:50:06 +0800

    enhance struct interface testing

--- mantisa/sample_code/src/struct_interface.go
+++ mantisa/sample_code/src/struct_interface.go
@@ -24,12 +24,21 @@ type Assassin struct {
     Character
 }
 
-func(p *Assassin) Move() int {
-    return 6
+type CriticalAttack struct {
+    Behavior  // used to initialize initial implementation from whatever passed in , and then override
 }
 
+func(c* CriticalAttack) Attack() string {
+    //return "Critical !"
+    return "Critical Attack! " + c.Behavior.Attack() // c.Behavior.Attack() can make magic that it behaves as whatever you passed in...
+}
+func SetCriticalAttack(src Behavior) Behavior {
+    return &CriticalAttack{src}
+}
+
+// only override Attack
 func(p *Assassin) Attack() string {
-    return "dagger"
+    return "Assassin dagger"
 }
 
 func(p *Ninja) Move() int {
@@ -37,16 +46,15 @@ func(p *Ninja) Move() int {
 }
 
 func(p *Ninja) Attack() string {
-    return "Shuriken"
+    return "Ninja Shuriken"
 }
 
 func(p *Mage) Move() int {
     return 2
 }
 func(p *Mage) Attack() string {
-    return "Fireball"
+    return "Mage Fireball"
 }
-
 
 func BadTakeAction1(p *Ninja) {
     fmt.Println("Bad TakeAction1: Ninja: Move=", p.Move(), ", Attack=", p.Attack())
@@ -67,11 +75,13 @@ func GoodTakeAction(p ...Behavior) {
 func main() {
     n := Ninja{Character{Name:"Orochi"}}
     m := Mage{Character{Name:"Sakura"}}
-    a := Assassin{}
+    a := Assassin{&Ninja{Character{Name:"Not_Important_Only_To_Take_Implementation"}}, Character{Name:"Taki"} }
+    var x = SetCriticalAttack(&Ninja{} ) // use NInja as default implementation, but override Attack (inspiration from GO reverse)
+
     BadTakeAction1(&n)
     BadTakeAction2(&n,&m)
-    //TakeAction(&n)  // note: & get address can be casted to pointers
-    //TakeAction(&m) 
-    //GoodTakeAction(&n, &m)
     GoodTakeAction(&n, &m, &a)
+    fmt.Println("--- demo changing different interface implementations on the fly ----")
+    GoodTakeAction(&Ninja{},  &Mage{}, x )
+
 }
